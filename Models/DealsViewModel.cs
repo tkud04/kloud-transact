51f69cb12f2af2bf664aa18e229d7806ef5c60b3
tkud04 <32499735+tkud04@users.noreply.github.com>
2019-03-17 14:30:29 +0100

    Add files via upload

--- Models/DealsViewModel.cs
+++ Models/DealsViewModel.cs
@@ -1,7 +1,5 @@
-using System;
-using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using System.Collections.Generic;
-using System.Linq;
 
 namespace kloud.Models
 {
