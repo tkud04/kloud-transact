5eefbd199e1e312800c65abfcdd715e034736ac8
tkud04 <32499735+tkud04@users.noreply.github.com>
2019-03-20 12:43:29 +0100

    Add files via upload

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,7 +2,7 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
-$('.shop-categories-link').click(function(e){
+$('.shop-categories-linkk').click(function(e){
 	e.preventDefault();
 	let c = $(this).attr('data-cat');
 	console.log("Category: " + c);
